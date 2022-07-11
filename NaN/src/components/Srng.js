84c8c0f0f771f50ed30e18e74060d780f6e26ea3
Sarang <sarang@clayfin.com>
2022-07-11 17:34:05 +0530

    ...

--- NaN/src/components/Srng.js
+++ NaN/src/components/Srng.js
@@ -1,18 +1,53 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Button, ScrollView } from 'react-native'
+import React, { useState } from 'react'
 import { arr } from './data'
 
 const Srng = () => 
-{
+{   
+    const [flag,setFlag]  = useState(false);
+    const [list,setList] = useState(arr);
+    const [obj,setObj] = useState({});
+
+
+    const handleplus = (j) =>
+    {
+        list.forEach((ele) =>
+        {
+            if(ele.id === j)
+            {
+                ele.No = ele.No+1
+                setFlag(!flag);
+                return;               
+            }
+        }) 
+        // console.log(list);
+    }
+
+    const handleminus = (j) =>
+    {
+        list.forEach((ele) =>
+        {
+            if(ele.id === j)
+            {
+                ele.No = ele.No-1;
+                setFlag(!flag);
+                return;               
+            }
+        })         
+    }
   
-    return (
-    <View>
-        {arr.map((e) =>
-        <View style={styles.parent}>
-            <Text style={styles.child}>{e.No}</Text>
-        </View>            
-        )}      
-    </View>
+    return (    
+        <ScrollView>
+            <View>
+            {list.map((e,k) =>
+                <View style={styles.parent}  key={k}>            
+                    <Button onPress={() => handleminus(e.id)} title="       -       "/>            
+                    <Text style={styles.child}>{e.No}</Text>            
+                    <Button onPress={() => handleplus(e.id)} title="       +       " />   
+                </View>     
+            )}      
+            </View>
+        </ScrollView>    
   )
 }
 
@@ -20,13 +55,24 @@ export default Srng
 
 const styles = StyleSheet.create({
     parent:
-    {
-        alignItems:'center',
-        borderWidth:'2px'
+    {           
+        flexDirection:'row',        
+        justifyContent:'center',        
+        height: 60,    
+        padding :10,
     },
     child:
     {
         textAlign:'center',
-        fontSize:28,        
+        fontSize:28,
+        fontWeight:'bold',
+        marginLeft:10,
+        marginRight:10,
+    },
+    btn:
+    {
+        width: 20,
+        height: 20,
+        
     }
 })
